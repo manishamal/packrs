0e7a7a3d34bd9019b17f8b19484068cc4e88b998
manishamal <malmanisha298@gmail.com>
2021-07-13 13:30:43 +0530

    fixed

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,8 +5,6 @@ const Section = styled.section`
   margin-bottom: 4rem;
   background-image: url(/images/about-app-bg.svg);
   background-repeat: no-repeat;
-  
-
 `;
 
 const Information = () => {
@@ -15,6 +13,12 @@ const Information = () => {
       <h1 className="has-text-centered is-size-2 has-text-weight-semibold has-text-black mb-5">
         Get it done with Packrs
       </h1>
+      <center>
+        <figure className="image is-128x128  ">
+          <img src="https://driveulu.com/uploads/media/5c4b31208436685436637d79/original-7c0b4e81246aa8097285422bc2fc3a26.png" />
+        </figure>
+      </center>
+
       <Section className="section hero is-medium">
         <div className="container" />
       </Section>
